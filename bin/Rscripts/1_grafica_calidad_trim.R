dc7607913a81032506d4d4b77bd942e9d72e8b04
Majo Monteverde <36015050+MajoMonteverde@users.noreply.github.com>
2018-05-25 11:37:57 -0500

    Update 1_grafica_calidad_trim.R
    English version

--- bin/Rscripts/1_grafica_calidad_trim.R
+++ bin/Rscripts/1_grafica_calidad_trim.R
@@ -1,12 +1,12 @@
-############ Gráfica calidad de seequencias Steptanthus después del trimming ##############
+############ Quality plot of Fastqc results of Streptanthus sequences after trimming ##############
 
-## Cargar libreria
+## Load library
 library(ggplot2)
 
-## Leer base de datos
+## Read data 
 dat <- read.csv("../strep_demulti_fastqs/4_fastqc_trim_qualitycharts/1_summary_trim.csv")
 
-## Graficar
+## Plot
 head (dat)
 dat2<-table(dat$Wrong)
 class(dat2)
@@ -14,27 +14,27 @@ class(dat2)
 dat2 <- as.data.frame(dat2)
 dat2 <- dat2[-c(1),]
 
-##Graficar
+##Plot
 ggplot(data=dat2, aes(x=Var1, y= Freq)) + geom_bar(stat="identity")
 myplot<-ggplot(data=dat2, aes(x=Var1, y= Freq))
 myplot + geom_bar(stat="identity")
 
-#aes cambiar color
+#Change color
 x <- ggplot(data=dat2, aes(x=Var1, y= Freq, color=Var1)) + geom_bar(stat="identity")
 ggplot(data=dat2, aes(x=Var1, y= Freq, color=Var1)) +
   geom_bar(stat="identity", aes(color=Var1, fill=Var1))
 
-##quitar fondo gris
+##Remove grey background
 y <- ggplot(data=dat2, aes(x=Var1, y= Freq, color=Var1)) +
   geom_bar(stat="identity", aes(color=Var1, fill=Var1)) + 
   theme_bw()
 
-##cambiar nombre de ejes
+##Change axis names
 z <- y + ylab("Frequency") + xlab("Problem")
 z  
 
-##quitar los nombres que tiene para cada barra
+##Remove names of each bar
 a <- z + theme(axis.text.x=element_blank())
 a
 
-sessionInfo()
+
