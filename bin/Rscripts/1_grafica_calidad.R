de3f4d110154fcb765f608f638f2a5aacb6bea33
Majo Monteverde <36015050+MajoMonteverde@users.noreply.github.com>
2018-05-25 11:35:53 -0500

    Update 1_grafica_calidad.R
    English version

--- bin/Rscripts/1_grafica_calidad.R
+++ bin/Rscripts/1_grafica_calidad.R
@@ -1,12 +1,12 @@
-############ Gráfica calidad de seequencias Steptanthus después del demultiplexeo ##############
+############ Quality plot of Fastqc results of Streptanthus sequences after demultiplexing ##############
 
-## Cargar libreria
+## Load library
 library(ggplot2)
 
-## Leer base de datos
+## Read csv summary
 dat <- read.csv("../strep_demulti_fastqs/2_fastqc_qualitycharts/1_summary.csv")
 
-## Graficar
+## Plot
 head (dat)
 dat2<-table(dat$Wrong)
 class(dat2)
@@ -14,27 +14,25 @@ class(dat2)
 dat2 <- as.data.frame(dat2)
 dat2 <- dat2[-c(1),]
 
-##Graficar
+## Plot
 ggplot(data=dat2, aes(x=Var1, y= Freq)) + geom_bar(stat="identity")
 myplot<-ggplot(data=dat2, aes(x=Var1, y= Freq))
 myplot + geom_bar(stat="identity")
 
-#aes cambiar color
+# Change color
 x <- ggplot(data=dat2, aes(x=Var1, y= Freq, color=Var1)) + geom_bar(stat="identity")
 ggplot(data=dat2, aes(x=Var1, y= Freq, color=Var1)) +
   geom_bar(stat="identity", aes(color=Var1, fill=Var1))
 
-##quitar fondo gris
+## Remove grey background
 y <- ggplot(data=dat2, aes(x=Var1, y= Freq, color=Var1)) +
   geom_bar(stat="identity", aes(color=Var1, fill=Var1)) + 
   theme_bw()
 
-##cambiar nombre de ejes
+## Change axis names
 z <- y + ylab("Frequency") + xlab("Problem")
 z  
 
-##quitar los nombres que tiene para cada barra
+## Rmeove names for each bar
 a <- z + theme(axis.text.x=element_blank())
 a
-
-sessionInfo()
